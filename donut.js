3cb09b46e8f890b19243943cbd0b447c9479efd6
Marcin N-K (buttitch) <marcin.nk@gmail.com>
2014-09-17 22:14:24 +0930

    Fixed so that the pie chart shows correct ratio

--- donut.js
+++ donut.js
@@ -1,12 +1,13 @@
 if (Meteor.isClient) {
     Meteor.startup(function () {
-        var donut = new EyePie({target: '#donut', data: [100, 0], color: ["#f00", "#00f"]});
+        var donut = new EyePie({target: '#donut', data: [], colour: ["#f00", "#00f"]});
 
         Deps.autorun(function () {
             var local = Session.get("counter");
             var global = Counters.findOne({_id: 'global'});
             if (global) {
-                donut.update([local, global.count]);
+                // update donut chart showing local to global clicks
+                donut.update([local, global.count - local]);
             }
         });
     });
